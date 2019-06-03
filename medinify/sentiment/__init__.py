cf6869bfc816b50d8ad72708cee185fc5d40463d
Gabby Gurdin <ggurdin@gmail.com>
2019-06-03 11:05:59 -0400

    Added beginnings of dataset class

--- medinify/sentiment/__init__.py
+++ medinify/sentiment/__init__.py
@@ -1,7 +1,9 @@
 """Configure medinify.scrapers
 """
 from medinify.sentiment.review_classifier import ReviewClassifier
+from medinify.sentiment.char_cnn_classifier import CharCnnDataset
 
 __all__ = (
     'ReviewClassifier',
+    'CharCnnDataset'
 )
---
+++ medinify/sentiment/char_cnn_classifier.py
@@ -0,0 +1,69 @@
+
+# Preprocessing
+import numpy, sys
+from numpy import array
+from sklearn.preprocessing import OneHotEncoder
+from string import printable
+
+# Pytorch
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+# Medinify
+from medinify.sentiment import ReviewClassifier
+
+
+class CharCnnDataset(Dataset):
+    """
+    Pytorch dataset class for sentiment analysis CharCNN
+    (As opposed to word level)
+    """
+
+    dataset = []
+    onehot_encoder = None
+
+    def __init__(self, dataset_file):
+
+        dataset = ReviewClassifier().create_dataset(dataset_file)
+        for review in dataset:
+            comment_text = ' '.join(list(review[0].keys()))
+            comment_rep = self.encode_comment(comment_text)
+
+            rating = review[1]
+            rating_rep = None
+            if rating == 'pos':
+                rating_rep = torch.tensor(1)
+            elif rating == 'neg':
+                rating_rep = torch.tensor(0)
+
+            self.dataset.append({'comment': comment_text, 'comment_tensor': comment_rep,
+                                 'rating': rating, 'rating_tensor': rating_rep})
+
+    def load_one_hot_encoder(self):
+        """
+        loads onehot_encoder for character vocabulary (ASCCI printables)
+        """
+        numpy.set_printoptions(threshold=sys.maxsize)
+
+        char_vocab = [x for x in printable]
+        values = array(char_vocab).reshape(len(char_vocab), 1)
+
+        self.onehot_encoder = OneHotEncoder(sparse=False, categories='auto',
+                                            handle_unknown='ignore', dtype=numpy.double)
+        self.onehot_encoder.fit_transform(values)
+
+    def encode_comment(self, comment):
+        """
+        Transforms string comment into one-hot encoded torch tensor
+        :param comment: comment string
+        :return: one-hot encoder torch tensor
+        """
+
+        torch.set_printoptions(threshold=5000)
+
+        if not self.onehot_encoder:
+            self.load_one_hot_encoder()
+
+        characters = array(list(comment)).reshape(len(comment), 1)
+        encoded = self.onehot_encoder.transform(characters)
+        return torch.from_numpy(encoded)
